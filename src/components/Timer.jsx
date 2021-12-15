33cbfc5ef8be65eb4d961ee65bf858e976e4e721
mcornale <massimiliano.cornale@gmail.com>
2021-12-15 20:53:29 +0100

    Add timer styles

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,7 +1,9 @@
+import styles from './Timer.module.css';
+
 const Timer = () => {
   return (
-    <div>
-      <h1>00:00</h1>
+    <div className={styles.timer}>
+      <h1 className={styles.timerValue}>00:00</h1>
       <h3>Restart</h3>
     </div>
   );
