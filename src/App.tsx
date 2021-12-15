4170f94dadee71590e5d89a58fd67fd3cd6432ea
mcornale <massimiliano.cornale@gmail.com>
2021-12-15 22:57:36 +0100

    Add settings icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import Button from './components/Button';
 import Logo from './components/Logo';
 import Timer from './components/Timer';
 import TimerSwitch from './components/TimerSwitch';
+import settingsIconSrc from './assets/icon-settings.svg';
 
 const App = () => {
   return (
@@ -8,6 +10,9 @@ const App = () => {
       <Logo />
       <TimerSwitch />
       <Timer />
+      <Button noOpacity>
+        <img src={settingsIconSrc} alt='settings icon' />
+      </Button>
     </main>
   );
 };
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,22 @@
 import styles from './Button.module.css';
 
 type Props = {
-  children: string;
+  children: string | JSX.Element;
+  noOpacity?: boolean;
 };
 
 const Button = (props: Props) => {
-  const { children } = props;
+  const { children, noOpacity } = props;
 
-  return <button className={styles.button}>{children}</button>;
+  return (
+    <button
+      className={`${styles.button} ${
+        noOpacity ? styles.buttonNoOpacity : styles.buttonOpacity
+      }`}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
