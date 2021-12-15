6bf7e8c9ddd02aea606ed03d6dddef9a98445c47
mcornale <massimiliano.cornale@gmail.com>
2021-12-15 22:46:11 +0100

    Finish timer design

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -3,10 +3,12 @@ import styles from './Timer.module.css';
 
 const Timer = () => {
   return (
-    <div className={styles.timer}>
-      <ProgressBar />
-      <h1 className={styles.timerValue}>00:00</h1>
-      <h3>Restart</h3>
+    <div className={styles.timerContainer}>
+      <div className={styles.timer}>
+        <ProgressBar />
+        <h1 className={styles.timerValue}>00:00</h1>
+        <h3>Restart</h3>
+      </div>
     </div>
   );
 };
