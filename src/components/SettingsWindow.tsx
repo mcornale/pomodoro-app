3747ae29d4434993e1075232ccb2fabde08101cd
mcornale <massimiliano.cornale@gmail.com>
2021-12-16 13:48:38 +0100

    Create static settings window component

--- src/components/SettingsWindow.tsx
+++ src/components/SettingsWindow.tsx
@@ -1,5 +1,55 @@
+import closeIconSrc from '../assets/icon-close.svg';
+import Button from './Button';
+
+import styles from './SettingsWindow.module.css';
+
 const SettingsWindow = () => {
-  return <></>;
+  return (
+    <section className={styles.settingsWindow}>
+      <header className={styles.settingsWindowHeader}>
+        <h2>Settings</h2>
+        <img src={closeIconSrc} alt='close icon' />
+      </header>
+      <form className={styles.settingsWindowForm}>
+        <div>
+          <h4>Time (minutes)</h4>
+          <div>
+            <div>
+              <label>pomodoro</label>
+              <input type='number' />
+            </div>
+            <div>
+              <label>short break</label>
+              <input type='number' />
+            </div>
+            <div>
+              <label>long break</label>
+              <input type='number' />
+            </div>
+          </div>
+        </div>
+        <div>
+          <h4>Font</h4>
+          <div>
+            <input type='radio' />
+            <input type='radio' />
+            <input type='radio' />
+          </div>
+        </div>
+        <div>
+          <h4>Color</h4>
+          <div>
+            <input type='radio' />
+            <input type='radio' />
+            <input type='radio' />
+          </div>
+        </div>
+        <Button type='submit' primary>
+          Apply
+        </Button>
+      </form>
+    </section>
+  );
 };
 
 export default SettingsWindow;
