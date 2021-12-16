07967ee2af6b3ff2a14fab55b9ac4f2e98cfea92
mcornale <massimiliano.cornale@gmail.com>
2021-12-16 17:38:26 +0100

    Add app costants

---
+++ src/constants.ts
@@ -0,0 +1,19 @@
+const TIMERS = {
+  POMODORO: 'pomodoro',
+  SHORT_BREAK: 'short break',
+  LONG_BREAK: 'long break',
+};
+
+const COLORS = {
+  ORANGE_RED: '#f87070',
+  TEAL: '#f87070',
+  PURPLE: '#d881f8',
+};
+
+const FONTS = {
+  KUMBH_SANS: 'Kumbh Sans',
+  ROBOTO_SLAB: 'Roboto Slab',
+  SPACE_MONO: 'Space Mono',
+};
+
+export { TIMERS, COLORS, FONTS };
