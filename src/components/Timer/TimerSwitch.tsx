b4b767137703bb11d7d964b51a477e51133ab521
mcornale <massimiliano.cornale@gmail.com>
2021-12-16 18:14:44 +0100

    Add animation to active timerswitch button

--- src/components/Timer/TimerSwitch.tsx
+++ src/components/Timer/TimerSwitch.tsx
@@ -8,8 +8,19 @@ import styles from './TimerSwitch.module.css';
 const TimerSwitch = () => {
   const { activeTimer, changeActiveTimer } = useContext(TimerContext);
 
+  const activeTimerIndex = Object.values(TIMERS).findIndex(
+    (value) => value === activeTimer
+  );
+
   return (
     <nav className={styles.timerSwitch}>
+      <div
+        className={styles.timerSwitchActiveBackground}
+        style={{
+          width: `calc(100% / ${Object.keys(TIMERS).length})`,
+          transform: `translate(calc(100% * ${activeTimerIndex}), -50%)`,
+        }}
+      ></div>
       {Object.entries(TIMERS).map(([timerKey, timerName]) => (
         <Button
           key={timerKey}
