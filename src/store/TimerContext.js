6d09b78b64c09f6325dd3fd36f9c72cff0c63d8c
mcornale <massimiliano.cornale@gmail.com>
2021-12-16 17:51:01 +0100

    Update context to use constants

--- src/store/TimerContext.js
+++ src/store/TimerContext.js
@@ -1,14 +1,16 @@
 import { createContext } from 'react';
+import { COLORS, FONTS, TIMERS } from '../constants';
 
 const TimerContext = createContext();
 
 const TimerContextProvider = (props) => {
   const timerContextValue = {
+    timerActive: TIMERS.POMODORO,
     pomodoroValue: 25,
     shortBreakValue: 5,
     longBreakValue: 15,
-    fontSelected: 'Kumbh Sans',
-    colorSelected: '#f87070',
+    fontSelected: FONTS.KUMBH_SANS,
+    colorSelected: COLORS.ORANGE_RED,
   };
 
   return (
